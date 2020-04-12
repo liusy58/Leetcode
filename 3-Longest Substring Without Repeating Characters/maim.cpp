f453327115eefe5ba7995931fd029d40cd8aa2a8
Nicholas <liusy58@mail2.sysu.edu.cn>
2020-04-12 16:55:25 +0800

    new

---
+++ 3-Longest Substring Without Repeating Characters/maim.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+    int start=0;
+    unordered_map<char,int>mp;
+
+
+    for(int i=0;i<s.size();++i)
+    {
+
+    }
+}
+int main()
+{
+    while(1)
+    {
+        string str;
+        cin>>str;
+        cout<<lengthOfLongestSubstring(str)<<endl;
+    }
+}
+
