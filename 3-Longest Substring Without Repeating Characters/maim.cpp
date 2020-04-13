54c041a5763e42ebf48ecfffa2ab0731f186879e
Nicholas <liusy58@mail2.sysu.edu.cn>
2020-04-13 12:59:20 +0800

    new

--- 3-Longest Substring Without Repeating Characters/maim.cpp
+++ 3-Longest Substring Without Repeating Characters/maim.cpp
@@ -5,12 +5,25 @@ using namespace std;
 int lengthOfLongestSubstring(string s) {
     int start=0;
     unordered_map<char,int>mp;
-
+    int res=0;
 
     for(int i=0;i<s.size();++i)
     {
-
+        if(!mp.count(s[i]))
+        {
+            mp[s[i]]=i+1;
+        }
+        else
+        {
+            if(mp[s[i]]>start)
+            {
+                start=mp[s[i]];
+                mp[s[i]]=i+1;
+            }
+        }
+        res=max(res,mp[s[i]]-start);
     }
+    return res;
 }
 int main()
 {
