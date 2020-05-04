566781e833c2092bff380da97413bf6f88e893f3
Nicholas <liusy58@mail2.sysu.edu.cn>
2020-05-04 08:54:18 +0800

    finish 42

--- 42/main.cpp
+++ 42/main.cpp
@@ -1,17 +1,29 @@
-       int trap(vector<int>& height)
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int trap(vector<int>& height)
 {
-    int res = 0, mx = 0, n = (int)height.size();
-    vector<int> dp(n, 0);
-    for (int i = 0; i != n; ++i)
+    int num=height.size();
+    if(!num)
+        return 0;
+    int res=0;
+    vector<int>before(num,0);
+    vector<int>after(num,0);
+    for(int i=1;i<num;++i)
+        before[i]=max(before[i-1],height[i-1]);
+    for(int i=num-2;i>=0;--i)
+        after[i]=max(after[i+1],height[i+1]);
+    for(int i=1;i<num-1;++i)
     {
-        dp[i] = mx;
-        mx = max(mx, height[i]);
+        int h=min(before[i],after[i]);
+        if(height[i]<h)
+            res+=h-height[i];
     }
-    mx = 0;
-    for (int i = n - 1; i >= 0; --i) {
-        dp[i] = min(dp[i], mx);
-        mx = max(mx, height[i]);
-        if (dp[i] > height[i]) res += dp[i] - height[i];
-    }
-    return res;
+    return res;     
+}
+
+int main()
+{
+
 }
