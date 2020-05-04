c9fc628529d7b3e287bf6e056bc1e658ac74e460
Nicholas <liusy58@mail2.sysu.edu.cn>
2020-05-04 17:09:01 +0800

    finish 48

---
+++ 48/main.cpp
@@ -0,0 +1,11 @@
+    void rotate(vector<vector<int>>& matrix) {
+        if(matrix.empty()||matrix[0].empty())
+            return;
+        int n=(int)matrix.size();
+        for(int i=0;i<n/2;++i)
+            for(int j=0;j<n;++j)
+                swap(matrix[i][j],matrix[n-1-i][j]);
+        for(int i=0;i<n;++i)
+            for(int j=0;j<i;++j)
+                swap(matrix[i][j],matrix[j][i]);
+    }
