37eb4df1aa70c0f84a357f7b30f062bd626d9726
Nicholas <liusy58@mail2.sysu.edu.cn>
2020-05-28 00:26:30 +0800

    finish

---
+++ 141/main.cpp
@@ -0,0 +1,13 @@
+    bool hasCycle(ListNode *head) {
+        
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast!=NULL&&fast->next!=NULL&&slow!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+                return true;
+        }
+        return false;
+    }
